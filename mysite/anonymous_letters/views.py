583881742c91cf70f3ac0814f6b7ff17e9e91d98
jravolio <jravolio892@gmail.com>
2022-01-28 12:18:41 -0300

    Front_end_changes

--- mysite/anonymous_letters/views.py
+++ mysite/anonymous_letters/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Letter
 
 # Create your views here.
+def post_list(request):
+    posts = Letter.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'anonymous_letters/post_list.html', {'posts': posts})
